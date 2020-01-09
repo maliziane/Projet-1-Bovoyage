e52aa5642672be943d8d2d2cb3c9dccfc4644a3d
adminl <adminl@localhost>
2020-01-09 17:02:25 +0100

    push

---
+++ src/fr/gtm/projet/voyage_mock/entities/ClientTest.java
@@ -0,0 +1,44 @@
+package fr.gtm.projet.voyage_mock.entities;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClientTest extends Client {
+
+	@Test
+	public void testGetNom() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetNom() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNumero() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetNumero() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClient() {
+		fail("Not yet implemented");
+	}
+
+}
