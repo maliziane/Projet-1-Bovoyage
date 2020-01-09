9b2bdf3bdbff4b7bfb5497a486850dcb4750ddcc
adminl <adminl@localhost>
2020-01-09 15:05:50 +0100

    3 eme version

--- src/fr/gtm/projet/voyage_mock/entities/Voyage.java
+++ src/fr/gtm/projet/voyage_mock/entities/Voyage.java
@@ -1,9 +1,46 @@
 package fr.gtm.projet.voyage_mock.entities;
 
-public class Voyage {
+import fr.gtm.voyage_mock.entities.Formule;
 
-	public Voyage() {
-		// TODO Auto-generated constructor stub
+public class Voyage {
+	
+	private String region;
+	private String descriptif;
+	private int id;
+	private Formule formule;
+	
+	public Voyage(String region, String descriptif, int id) {
+		this.region = region;
+		this.descriptif = descriptif;
+		this.id = id;
+	}
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+	public String getDescriptif() {
+		return descriptif;
+	}
+
+	public void setDescriptif(String descriptif) {
+		this.descriptif = descriptif;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public double getPrix() {
+		return formule.getPrix();
 	}
 
 }
