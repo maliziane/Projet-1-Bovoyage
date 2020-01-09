8f1face610a0de3d408d28736acf2168a7c1fc60
adminl <adminl@localhost>
2020-01-09 15:47:57 +0100

    JUnit DestinationTest All OK

--- src/fr/gtm/projetTest/DestinationTest.java
+++ src/fr/gtm/projetTest/DestinationTest.java
@@ -2,60 +2,67 @@ package fr.gtm.projetTest;
 
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.Test;
 
+import fr.gtm.projet.destination_mock.DAO.DestinationMockDao;
 import fr.gtm.projet.destination_mock.entities.Destination;
+import fr.gtm.projet.destination_mock.entities.Formule;
 
 public class DestinationTest {
 
 	@Test
-	public void testAddImage(String image) {
+	public void testAddImage() {
 		Destination d = new Destination("Paris");
-		assertNotNull(d.getNom());
-		
-	}}
+		d.addImage("toto");
+		assertEquals("toto",d.getImages().get(0));
+	}
 
-//	@Test
-//	public void testAddFormule() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testDestination() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testGetNom() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testSetNom() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testGetDescription() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testSetDescription() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testGetId() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testSetId() {
-//		fail("Not yet implemented");
-//	}
+	@Test
+	public void testAddFormule() {
+		Destination d = new Destination("Paris");
+		Formule f = new Formule();
+		d.addFormule(f);
+		assertEquals(f,d.getFormules().get(0));
+	}
+
+	@Test
+	public void testGetNom() {
+		Destination d = new Destination("Paris");
+		assertEquals("Paris",d.getNom());
+	}
+
+	@Test
+	public void testSetNom() {
+		Destination d = new Destination("Paris");
+		d.setNom("Lyon");
+		assertEquals("Lyon", d.getNom());
+	}
+
+		
+
+	@Test
+	public void testSetDescription() {
+		Destination d = new Destination("Paris");
+		d.setDescription("alpha");
+		assertEquals("alpha",d.getDescription());
+	
+	}
+	@Test
+	public void testgetId() {
+		Destination d = new Destination("Paris");
+		assertNull(d.getId());
+	}
+	
+	@Test
+	public void testSetId() {
+		Destination d = new Destination("Paris");
+		DestinationMockDao dao = new DestinationMockDao();
+		dao.creer(d);
+		assertNotNull(d.getId());
+		Long g=10L;
+		d.setId(g);
+		assertEquals(g,d.getId());
+	}
 //
 //}
+}
