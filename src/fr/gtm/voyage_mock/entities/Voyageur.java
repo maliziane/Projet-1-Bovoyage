959f669b95834b0bd89e3bba8451b2f60e25904d
adminl <adminl@localhost>
2020-01-09 15:01:00 +0100

    Ajout du voyage

--- src/fr/gtm/voyage_mock/entities/Voyageur.java
+++ src/fr/gtm/voyage_mock/entities/Voyageur.java
@@ -1,9 +1,24 @@
 package fr.gtm.voyage_mock.entities;
 
+import java.util.Date;
+
 public class Voyageur {
+	
+	private String civilite;
+	private String nom;
+	private String prenom;
+	private Date dateNaisance;
+	private String numero;
+	private Long id;
+	
 
-	public Voyageur() {
-		// TODO Auto-generated constructor stub
+	public Voyageur(String civilite, String nom, String prenom, Date dateNaisance, String numero, Long id) {
+		this.civilite = civilite;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.dateNaisance = dateNaisance;
+		this.numero = numero;
+		this.id = id;
 	}
-
+	
 }
